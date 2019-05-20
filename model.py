c357df18738ace7c786307253c1b956f29c4ccd8
p-s-vishnu <psvishnu.it13@gmail.com>
2019-05-20 23:25:28 +0530

    feat:default output JSON

--- model.py
+++ model.py
@@ -1,10 +1,15 @@
 from flask import Flask
+
+# Initialize Flask object
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    return "Hello App!"
+def default():
+    return "{\"result\":{\"prediction\":1,\"probability\": 0.75}}"
 
+@app.route("/calculate")
+def calculate():
+    return "Work in progress"
 
 if __name__ == '__main__':
     app.run()
