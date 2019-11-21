966d2200635fd2e99673399e6f6b26d96bf61619
PS Vishnu <psvishnu.it13@gmail.com>
2019-11-21 16:08:34 +0530

    Version 1 (#6)
    * fix: remove used dependencies
    
    * feat: default output JSON
    
    * feat: add another model to flask API
    
    * Docs: data visualization
    
    * update:swagger ui
    
    * refactor: fix merge conflict
    
    * Add flasgger requirement
    
    * refactor: add pandas requirement
    
    * refactor: change scikit version
    
    * update: model change
    
    * refactor: import model
    
    * update:restructure directories

---
+++ app.py
@@ -0,0 +1,64 @@
+from flask import Flask, request
+from flasgger import Swagger
+from pickle import load
+import pandas as pd
+
+file_name = './model/model.pkl'
+# read as binary
+with open(file_name, 'rb') as model_pkl:
+    clf = load(model_pkl)
+
+# Initialize the Flask object
+app = Flask(__name__)
+swagger = Swagger(app)
+
+# @app.route("/")
+# def root():
+#     return """This is the root of the API, use the following APIs as per your need<br/>
+#         1. url/test: It will return a dummy result
+#             with the correct response structure<br/>
+#         2. url/calculate [POST]: This is the actual API used for
+#         predicting the loan status and the probabilistic value"""
+
+
+# @app.route("/test")
+# def test():
+#     return "{\"result\":{\"prediction\":1,\"probability\":0.75}}"
+
+
+# @app.route("/calculate", methods=['POST'])
+@app.route("/calculate")
+def get_output():
+    """Example endpoint returning a prediction of loan status.
+    ---
+    parameters:
+      - name: ApplicantIncome
+        in: query
+        type: number
+        required: true
+      - name: CoapplicantIncome
+        in: query
+        type: number
+        required: true
+    responses:
+      500:
+        description: Error!
+      200:
+        description: Loan status predicted
+        
+    """
+    # a = request.form['a']
+    # b = request.form['b'].
+    a = request.args.get('ApplicantIncome')
+    b = request.args.get('CoapplicantIncome')
+
+    return str(clf.predict([[float(a), float(b)]]))
+  
+@app.route("/calculate_file", methods=['POST'])
+def get_file_output():
+    df = pd.read_csv(request.files.get('input_file'),header=None)
+    return str(list(clf.predict(df)))
+
+
+if __name__ == '__main__':
+    app.run()
---
+++ model/model.py
@@ -0,0 +1,37 @@
+ # import necessary files
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
+from os import listdir
+import pickle
+
+# load data
+path = '../data/'
+df = pd.read_csv(path+'train.csv')
+X = df[['ApplicantIncome', 'CoapplicantIncome']]
+y = df[['Loan_Status']].apply(lambda x: x.replace({"Y":1,"N":0}))
+print(y.shape)
+
+# split into test train
+X_train, X_test, y_train, y_test = train_test_split(X, np.ravel(y),
+                                                    random_state=42,
+                                                    test_size=0.2)
+
+# build model
+clf = RandomForestClassifier(n_estimators=10)
+
+# train the classifier
+clf.fit(X_train, y_train)
+
+# predictions
+predicted = clf.predict(X_test)
+
+# accuracy
+print(accuracy_score(y_test, predicted))
+
+# pickle the model
+# create a file object called model_pkl
+with open('model.pkl', 'wb') as model_pkl:
+    pickle.dump(clf, model_pkl)
